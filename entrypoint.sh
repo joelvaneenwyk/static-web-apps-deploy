eaa3a1869a8650c1648f474b7563f53203a919a6
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-04-20 10:18:42 -0700

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
-#!/bin/sh -l
+#!/usr/bin/env sh
 cd /bin/staticsites/ || true
 
 export HUGO_VERSION="${HUGO_VERSION:-0.122.0}"
 
 if [ -z "${INPUT_ACTION}" ] && [ -z "$*" ]; then
-    echo "No action specified, defaulting to build"
-    export INPUT_ACTION="build"
-    ./StaticSitesClient "${INPUT_ACTION}"
+  echo "No action specified, defaulting to build"
+  export INPUT_ACTION="build"
+  ./StaticSitesClient "${INPUT_ACTION}"
 else
-    ./StaticSitesClient "$@"
+  ./StaticSitesClient "$@"
 fi
