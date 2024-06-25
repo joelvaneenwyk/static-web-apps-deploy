d0238e7af6412792c2542da47687fd14445b0c51
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-06-25 10:50:17 -0700

    Push docker image after build and run simple test
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,47 +13,43 @@
 #
 # Options:
 #
-#  --verbose                       (Default: false) Enables verbose logging
-#  --deploymentaction              (Default: ) Specifies the action to run
-#  --workdir                       (Default: ) Working directory of the repository
-#  --buildTimeoutInMinutes         (Default: ) Time limit of oryx build in minutes
-#  --app                           (Default: ) Directory of app source code
 #  --api                           (Default: ) Directory of api source code
-#  --dataApi                       (Default: ) Directory of data api configuration files
-#  --routesLocation                (Default: ) Path to the routes file
-#  --configFileLocation            (Default: ) Path to the staticwebapp.config.json file
-#  --outputLocation                (Default: ) Directory of built application artifacts
-#  --appArtifactLocation           (Default: ) Directory of built application artifacts
-#  --event                         (Default: ) Filepath of the event json
-#  --apiToken                      (Default: ) ApiToken
-#  --repoToken                     (Default: ) RepoToken
-#  --appBuildCommand               (Default: ) App Build Command
 #  --apiBuildCommand               (Default: ) Api Build Command
-#  --repositoryUrl                 (Default: ) Repository Url
-#  --deploymentEnvironment         (Default: ) Deployment Environment
+#  --apiToken                      (Default: ) ApiToken
+#  --app                           (Default: ) Directory of app source code
+#  --appArtifactLocation           (Default: ) Directory of built application artifacts
+#  --appBuildCommand               (Default: ) App Build Command
 #  --branch                        (Default: ) Branch
+#  --buildTimeoutInMinutes         (Default: ) Time limit of oryx build in minutes
+#  --configFileLocation            (Default: ) Path to the staticwebapp.config.json file
+#  --dataApi                       (Default: ) Directory of data api configuration files
+#  --deploymentaction              (Default: ) Specifies the action to run
+#  --deploymentEnvironment         (Default: ) Deployment Environment
 #  --deploymentProvider            (Default: ) Deployment provider
-#  --skipAppBuild                  (Default: false) Skips Oryx build for app folder
-#  --skipApiBuild                  (Default: false) Skips Oryx build for api folder
-#  --skipDeployOnMissingSecrets    (Default: false) Skips deployment if api token isn't specified.
-#  --isStaticExport                (Default: false) Repository uses Static Export of Site
-#  --pullRequestTitle              Pull request title for staging sites.
+#  --event                         (Default: ) Filepath of the event json
 #  --headBranch                    Head branch name for staging sites.
-#  --productionBranch              Production branch. When specified, deployments from other branches will be staging environments.
 #  --help                          Display this help screen.
+#  --isStaticExport                (Default: false) Repository uses Static Export of Site
+#  --outputLocation                (Default: ) Directory of built application artifacts
+#  --productionBranch              Production branch. When specified, deployments from other branches will be staging environments.
+#  --pullRequestTitle              Pull request title for staging sites.
+#  --repositoryUrl                 (Default: ) Repository Url
+#  --repoToken                     (Default: ) RepoToken
+#  --routesLocation                (Default: ) Path to the routes file
+#  --skipApiBuild                  (Default: false) Skips Oryx build for api folder
+#  --skipAppBuild                  (Default: false) Skips Oryx build for app folder
+#  --skipDeployOnMissingSecrets    (Default: false) Skips deployment if api token isn't specified.
+#  --verbose                       (Default: false) Enables verbose logging
 #  --version                       Display version information.
+#  --workdir                       (Default: ) Working directory of the repository
 #
 
 set -eax -o pipefail
 
 echo "##[cmd] $(realpath "$0" || readlink -f "$0" || $0) $*"
-for a in "$@"; do
-  printf "%s" "$a";
+for argument in "$@"; do
+  printf "%s" "$argument"
 done
-
-export HUGO_VERSION="${HUGO_VERSION:-0.127.0}"
-export INPUT_ACTION="${INPUT_ACTION:-${1:-build}}"
-export BUILD_COMMAND_OVERRIDE="${1:-}"
 
 export SWA_DIR="${SWA_DIR:-/bin/staticsites}"
 if [ ! -e "${SWA_DIR:-}/StaticSitesClient" ]; then
@@ -61,18 +57,39 @@ if [ ! -e "${SWA_DIR:-}/StaticSitesClient" ]; then
 fi
 export SWA="${SWA_DIR}/StaticSitesClient"
 
+export HUGO_VERSION="${HUGO_VERSION:-0.127.0}"
+export BUILD_COMMAND_OVERRIDE="${1:-}"
+export ACTION=""
+
 ARGS=()
-if [[ -n "$*" ]] || [[ X${BUILD_COMMAND_OVERRIDE:-} == Xsh* ]] || [[ X${BUILD_COMMAND_OVERRIDE:-} == Xbash* ]]; then
-  echo "No action specified, passing all arguments to shell directly."
-  ARGS+=("$@")
-else
-  if [[ "${INPUT_ACTION:-}" = "${BUILD_COMMAND_OVERRIDE:-}" ]]; then
-    shift
+COMMANDS=(run upload close help version)
+IS_OVERRIDE=0
+
+for argument in "$@"; do
+  if [ ${#ARGS[@]} -eq 0 ]; then
+    if [[ X${argument:-} == Xsh* ]] || [[ X${argument:-} == Xbash* ]]; then
+      echo "No action specified, passing all arguments to shell directly."
+      IS_OVERRIDE=1
+    else
+      ARGS+=("${SWA}")
+    fi
   fi
-  ARGS+=("${SWA}" "${INPUT_ACTION}")
-  if [[ -n "$*" ]]; then
-    ARGS+=("$@")
+
+  ARGS+=("${argument}")
+
+  if [[ " ${COMMANDS[*]} " =~ [[:space:]]${argument}[[:space:]] ]]; then
+    export ACTION="${argument}"
   fi
+done
+
+if [[ "$IS_OVERRIDE" == "0" ]] && [[ -z "$ACTION" ]]; then
+  export ACTION="run"
+  ARGS+=("${ACTION}")
+  echo "[WARNING] No action specified so appended default 'run' action."
+fi
+
+if [[ "$IS_OVERRIDE" == "0" ]] && [[ ! "$ACTION" = "version" ]] && [[ ! " ${ARGS[*]} " =~ [[:space:]]--verbose[[:space:]] ]]; then
+  ARGS+=(--verbose)
 fi
 
 if [ ! -f "${SWA}" ]; then
